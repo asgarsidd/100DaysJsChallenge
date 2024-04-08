5b2c648e25bbc58da4c3c198ea69cc523e5aeb42
asgarsidd <asgar2feb@gmail.com>
2024-04-08 21:14:22 +0530

    Find Average of Array Interview question for Fresher

---
+++ Day9_AverageFind.js
@@ -0,0 +1,19 @@
+const averageFind = (arr) => {
+  // Preferable way for Web Dev
+  const arr2 = arr.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+  return arr2 / arr.length;
+  // Through looping (naive)👇
+  let sum = null;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.length > 1) {
+      sum += arr[i];
+    } else return arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(averageFind([4, 2, 5, 1])); //Output:3
+console.log(averageFind([5, 10, 2, 8])); //Output:6.25
+console.log(averageFind([4])); //Output:4
