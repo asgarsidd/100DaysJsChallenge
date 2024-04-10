9931a4e0179f192ba0eb8235e59b569a430a91bb
asgarsidd <asgar2feb@gmail.com>
2024-04-10 19:24:53 +0530

    Sum of Digit Interview Question

---
+++ Day11_SumofDigit.js
@@ -0,0 +1,13 @@
+const sumOfDigits = (str) => {
+  let arr = Array.from(String(str), Number);
+  console.log(arr);
+  const sum = arr.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
+  console.log(sum);
+  return sum;
+};
+
+console.log(sumOfDigits(1234)); //Output: 10
+console.log(sumOfDigits(5434)); //Output: 16
+console.log(sumOfDigits(120384)); //Output: 18
