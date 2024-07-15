3b4f029e5576c1939bd8fae854eb803f4b573188
asgarsidd <asgar2feb@gmail.com>
2024-07-15 22:53:14 +0530

    Array of Zeroes (Manipulation Array) in javasscript interview Qestion #Day95

--- Day95_ArrayEquals_of_Zero.js
+++ Day95_ArrayEquals_of_Zero.js
@@ -2,7 +2,7 @@ function ArrayOfZeroes(N, arr) {
   for (let i = 0; i < N - 1; i++) {
     // If current element is odd
     if (arr[i] % 2 !== 0) {
-      // Decrement both arr[i] and arr[i+1]
+      // Decrement both arr[i] and  arr[i+1]
       arr[i]--;
       arr[i + 1]--;
     }
@@ -16,7 +16,7 @@ function ArrayOfZeroes(N, arr) {
   }
 }
 
-// Test cases:
+ // Test cases:
 console.log(ArrayOfZeroes(5, [2, 3, 4, 3, 2])); // Expected output: YES
 console.log(ArrayOfZeroes(3, [1, 1, 1])); // Expected output: NO
 console.log(ArrayOfZeroes(6, [4, 4, 4, 4, 4, 4])); // Expected output: YES
