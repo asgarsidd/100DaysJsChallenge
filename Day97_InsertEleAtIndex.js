d044d544fdf36f57df049abfed3f8cabdccd04c3
asgarsidd <asgar2feb@gmail.com>
2024-07-30 23:18:21 +0530

    extract books name from Array of objects and store in Array in javascript interview question Day98

--- Day97_InsertEleAtIndex.js
+++ Day97_InsertEleAtIndex.js
@@ -1,21 +1,35 @@
 /**
- * Day97- How can we insert an element  a specific index in an Array?
-*/
+ * ?  Day97- How can we insert an element  a specific index in an Array?
+ * !  - - - - - - - - - - - - - - - - - -
+ * TODO:- DAY97 of 100DaysOfCode - - - - - - - - - - -
+ */
 const insertAtIndex = (arr, index, value) => {
-  // Method 1: using For loop :-(
+  /**
+   * ! Method 1: using For loop :-(
+   */
+
   let newArr = [];
   for (let i = 0; i < arr.length; i++) {
     if (i === index) {
       newArr.push(value);
     }
-   
+
     newArr.push(arr[i]);
   }
-  // Method 2: Using Splice method :-(
-   const newSplice = [...arr];
-    newSplice.splice(2,0,3);
-  return {newArr, newSplice};
+  /**
+   * ! Method 2: Using Splice method :-(
+   */
+
+  const newSplice = [...arr];
+  newSplice.splice(2, 0, 3);
+  return { newArr, newSplice };
 };
 
-console.log(insertAtIndex([1, 2, 4], 2, 3)); //{ newArr: [ 1, 2, 3, 4 ], newSplice: [ 1, 2, 3, 4 ] }
-console.log(insertAtIndex([1, 2,6,9], 2, 3)); //{ newArr: [ 1, 2, 3, 6, 9 ], newSplice: [ 1, 2, 3, 6, 9 ] }
+console.log(insertAtIndex([1, 2, 4], 2, 3));
+/**
+ * ! Output :-- { newArr: [ 1, 2, 3, 4 ], newSplice: [ 1, 2, 3, 4 ] }
+ */
+console.log(insertAtIndex([1, 2, 6, 9], 2, 3));
+/**
+ * ? Output :-- { newArr: [ 1, 2, 3, 6, 9 ], newSplice: [ 1, 2, 3, 6, 9 ] }
+ */
---
+++ Day98_FilterByBookInObjects.js
@@ -0,0 +1,34 @@
+/**
+ * ? Day98- Collect books from array of objects and return collection of books as an array 
+ * !  - - - - - - - - - - - - - - - - - -
+ * TODO:- DAY98 of 100DaysOfCode - - - - - - - - - - -
+ */
+
+let friends = [
+    {
+        name: 'Anna',
+        books: ['Bible', 'Harry Potter'],
+        age: 21
+    },
+    {
+        name: 'Alice',
+        books: ['War and Peace', 'Deep Work'],
+        age: 24
+    },
+    {
+        name: 'Bob',
+        books: ['Power of Habit', 'Sapiens'],
+        age: 26
+    }
+]
+/**
+ * ! Extract all the books from this object in Array!!
+ */
+let allBooks = friends.reduce((pre, curr)=>{
+    return[...pre, ...curr.books]
+},[])
+console.log(allBooks);
+/**
+ * TODO : Output---->> 
+ * !['Bible', 'Harry Potter', 'War and Peace', 'Deep Work', 'Power of Habit', 'Sapiens']
+ */
